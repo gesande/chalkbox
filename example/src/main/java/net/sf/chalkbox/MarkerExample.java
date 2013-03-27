db1873d7a3649cbf035f9b7de45b26db9ff34c40
gsande <geir.sande@luukku.com>
2013-03-27 17:23:43 +0000

    two separate examples how to use chalk and marker

--- example/src/main/java/net/sf/chalkbox/MarkerExample.java
+++ example/src/main/java/net/sf/chalkbox/MarkerExample.java
@@ -4,40 +4,12 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import org.junit.Test;
-
-public class Example {
-
-    @SuppressWarnings("static-method")
-    @Test
-    public void chalk() throws IOException {
-        final ChalkBox box = new ChalkBox();
-        final StringBuilder sb = new StringBuilder();
-        appendText(sb, box.black(), "black");
-        appendText(sb, box.red(), "red");
-        appendText(sb, box.green(), "green");
-        appendText(sb, box.yellow(), "yellow");
-        appendText(sb, box.blue(), "blue");
-        appendText(sb, box.magenta(), "magenta");
-        appendText(sb, box.cyan(), "cyan");
-        appendText(sb, box.white(), "white");
-
-        final FileOutputStream out = new FileOutputStream(new File("./target",
-                "chalk-example"));
-        final String content = sb.toString();
-        System.out.println(content);
-        try {
-            out.write(content.getBytes());
-        } finally {
-            out.flush();
-            out.close();
-        }
-
+public class MarkerExample {
+    public static void main(String[] args) throws IOException {
+        marker();
     }
 
-    @SuppressWarnings("static-method")
-    @Test
-    public void marker() throws IOException {
+    public static void marker() throws IOException {
         final ChalkBox box = new ChalkBox();
         final StringBuilder sb = new StringBuilder();
         final Marker marker = new Marker();
@@ -61,7 +33,6 @@ public class Example {
             out.flush();
             out.close();
         }
-
     }
 
     private static void appendText(final StringBuilder sb, final Chalk chalk,
@@ -69,12 +40,8 @@ public class Example {
         appendText(sb, marker.mark(chalk.write(text)));
     }
 
-    private static void appendText(final StringBuilder sb, final Chalk chalk,
-            final String text) {
-        appendText(sb, chalk.write(text));
-    }
-
     private static void appendText(final StringBuilder sb, final String text) {
         sb.append("this is ").append(text).append(" text").append("\n");
     }
+
 }
