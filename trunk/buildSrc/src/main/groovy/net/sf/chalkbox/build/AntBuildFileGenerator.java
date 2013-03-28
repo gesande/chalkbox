c38d3208a64bd2ff2946092afdcbc45fe7339882
gsande <gsande@3dc70175-81dd-419d-beed-10747ed95c4d>
2013-03-28 12:18:06 +0000

    gradle exportAntBuildFile

---
+++ trunk/buildSrc/src/main/groovy/net/sf/chalkbox/build/AntBuildFileGenerator.java
@@ -0,0 +1,99 @@
+package net.sf.chalkbox.build;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class AntBuildFileGenerator {
+
+    public void generate(final File parent, final String defaultTarget,
+            final String... targets) {
+        if (!parent.exists()) {
+            throw new RuntimeException("Given parent directory doesn't exist!");
+        }
+        writeBuildFile(parent, contentsOfAntBuildFile(defaultTarget, targets));
+    }
+
+    private StringBuilder contentsOfAntBuildFile(final String defaultTarget,
+            final String... targets) {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("<project name=\"gradle\" default=\"").append(defaultTarget)
+                .append("\" basedir=\"..\\\">").append(newLine());
+        sb.append(tab())
+                .append("<description description=\"Build entrypoints\">")
+                .append(newLine()).append(tab()).append("</description>")
+                .append(newLine());
+        sb.append(tab())
+                .append("<!-- set global properties for this build -->")
+                .append(newLine());
+        sb.append(tab()).append("<property environment=\"env\" />")
+                .append(newLine());
+        sb.append(tab())
+                .append("<property name=\"gradle.executable\" location=\"${env.GRADLE_HOME}/bin/gradle\" />")
+                .append(newLine());
+
+        if(defaultTarget!=null && !defaultTarget.isEmpty()) { 
+            appendTarget(sb, defaultTarget);
+        }
+        for (final String target : targets) {
+            appendTarget(sb, target);
+        }
+        return sb.append("</project>").append(newLine());
+    }
+
+    private void appendTarget(final StringBuilder sb, final String target) {
+        sb.append(tab()).append("<target name=\"").append(target)
+                .append("\">").append(newLine());
+        sb.append(tab())
+                .append(tab())
+                .append("<exec executable=\"${gradle.executable}\" dir=\".\">")
+                .append(newLine());
+        sb.append(tab()).append(tab()).append(tab())
+                .append("<arg value=\"").append(target).append("\" />")
+                .append(newLine());
+        sb.append(tab()).append(tab()).append("</exec>").append(newLine());
+        sb.append(tab()).append("</target>").append(newLine());
+    }
+
+    private String newLine() {
+        return "\n";
+    }
+
+    private static String tab() {
+        return "\t";
+    }
+
+    private static BufferedWriter newBufferedWriter(final File buildFile) {
+        try {
+            return new BufferedWriter(new FileWriter(buildFile));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void writeBuildFile(final File file,
+            final StringBuilder contents) {
+        writeBuildFile(newBufferedWriter(newBuildFile(file)), contents);
+    }
+
+    private static File newBuildFile(final File file) {
+        return new File(file, "chalkbox.xml");
+    }
+
+    private static void writeBuildFile(final BufferedWriter out,
+            final StringBuilder javaProjectContents) {
+        try {
+            out.write(javaProjectContents.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
