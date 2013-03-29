406a77230b9bef11b7bc444f36c312994da9d5d1
gsande <gsande@3dc70175-81dd-419d-beed-10747ed95c4d>
2013-03-29 15:37:44 +0000

    buildfile-generator module introduced, also fixed build.gradle generator bug

--- trunk/buildSrc/src/main/groovy/net/sf/chalkbox/build/GradleBuildFileGenerator.java
+++ trunk/buildSrc/src/main/groovy/net/sf/chalkbox/build/GradleBuildFileGenerator.java
@@ -39,7 +39,7 @@ public class GradleBuildFileGenerator {
 
     private static StringBuilder javaProjectContents(final String name) {
         final StringBuilder sb = new StringBuilder("project(':").append(name)
-                .append("' {").append("\n")
+                .append("') {").append("\n")
 
                 .append(tab()).append("apply from: \"$emmaPlugin\"")
                 .append("\n")
