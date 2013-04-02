f5959acd77c9e6ea9bc905f249def1bb946fceb9
gsande <gsande@3dc70175-81dd-419d-beed-10747ed95c4d>
2013-04-02 11:45:39 +0000

    created gradle tasks for build stuff

---
+++ trunk/buildSrc/src/main/groovy/net/sf/chalkbox/build/ExportGradleBuildFileForNewJavaProject.groovy
@@ -0,0 +1,18 @@
+package net.sf.chalkbox.build;
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+import org.gradle.logging.StyledTextOutputFactory
+import org.gradle.logging.StyledTextOutput.Style
+
+public class ExportGradleBuildFileForNewJavaProject extends DefaultTask{
+    def parent
+    def projectName
+
+    @TaskAction
+    def exportBuildFile() {
+        def GradleBuildFileGenerator generator =new GradleBuildFileGenerator()
+        generator.forJavaProject(parent, projectName)
+        services.get(StyledTextOutputFactory).create("buildGradleForJavaProject").withStyle(Style.Info).println("build.gradle successfully created for project '${projectName}'.")
+    }
+}
