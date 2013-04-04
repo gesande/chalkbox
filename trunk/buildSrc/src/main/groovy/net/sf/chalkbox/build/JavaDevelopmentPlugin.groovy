3b59bf32daf14b5d6fadc8a6510c9dfd76131aa0
gsande <gsande@3dc70175-81dd-419d-beed-10747ed95c4d>
2013-04-04 11:10:19 +0000

    JavaDevelopmentPlugin changes

--- trunk/buildSrc/src/main/groovy/net/sf/chalkbox/build/JavaDevelopmentPlugin.groovy
+++ trunk/buildSrc/src/main/groovy/net/sf/chalkbox/build/JavaDevelopmentPlugin.groovy
@@ -10,7 +10,6 @@ public class JavaDevelopmentPlugin implements Plugin<Project> {
 
     @Override
     public void apply(final Project project) {
-
         project.task("createLibDirs") { Task task ->
             group 'Development'
             description 'Creates lib and lib-sources -directories under the given project.'
