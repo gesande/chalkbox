bcf91829b3c557893ee847202b6c6be2e3a31ca2
gsande <geir.sande@luukku.com>
2013-04-04 13:09:00 +0000

    gradle applySvnIgnore to JavaDevelopmentPlugin

--- buildSrc/src/main/groovy/net/sf/chalkbox/build/JavaDevelopmentPlugin.groovy
+++ buildSrc/src/main/groovy/net/sf/chalkbox/build/JavaDevelopmentPlugin.groovy
@@ -6,6 +6,7 @@ import org.gradle.api.Task
 import org.gradle.logging.StyledTextOutputFactory
 import org.gradle.logging.StyledTextOutput.Style
 
+
 public class JavaDevelopmentPlugin implements Plugin<Project> {
 
     @Override
@@ -47,6 +48,72 @@ public class JavaDevelopmentPlugin implements Plugin<Project> {
                 services.get(StyledTextOutputFactory).create("java-development.addProjectToSvn").withStyle(Style.Info).println("Project ${project.name} was added to SVN")
             }
         }
+
+        project.task("generateSvnIgnoreFile") {
+            group 'Development'
+            description "Generates SVN ignore file."
+            doLast {
+                def StringBuilder sb = new StringBuilder()
+                sb.append("build\n")
+                sb.append("target\n")
+                sb.append(".project\n")
+                sb.append(".classpath\n")
+                sb.append(".settings\n")
+                sb.append(".pmd\n")
+                sb.append(".gradle\n")
+                def File svnIgnore = project.file("svn-ignore")
+                FileOutputStream output = new FileOutputStream(svnIgnore)
+                output.write(sb.toString().getBytes())
+            }
+        }
+
+        project.task("applySvnIgnoreFromGeneratedFile") { Task task ->
+            task.dependsOn("generateSvnIgnoreFile")
+            group 'Development'
+            description "Applies SVN ignore based on contents in project's svn-ignore file."
+            doLast {
+                def out= ""
+                new ByteArrayOutputStream().withStream { os ->
+                    def result =project.exec {
+                        executable = 'svn'
+                        args = [
+                            'propset',
+                            'svn:ignore',
+                            '-F',
+                            "${task.project.projectDir}/svn-ignore",
+                            "${task.project.name}"
+                        ]
+                        standardOutput = os
+                    }
+                    out = os.toString()
+                }
+                services.get(StyledTextOutputFactory).create("java-development.applySvnIgnore").withStyle(Style.Info).println("Svn ignore applied as defined in ${task.project.projectDir}/svn-ignore in project ${task.project.name}.")
+            }
+        }
+
+        project.task("applySvnIgnore") { Task task ->
+            group 'Development'
+            description "Applies SVN ignore based on contents in ${project.properties.svnIgnoreFile}."
+            doLast {
+                def out= ""
+                new ByteArrayOutputStream().withStream { os ->
+                    def result = project.exec {
+                        executable = 'svn'
+                        args = [
+                            'propset',
+                            'svn:ignore',
+                            '-F',
+                            "${project.properties.svnIgnoreFile}",
+                            "${task.project.name}"
+                        ]
+                        standardOutput = os
+                    }
+                    out = os.toString()
+                }
+                services.get(StyledTextOutputFactory).create("java-development.applySvnIgnore").withStyle(Style.Info).println("Svn ignore applied as defined in ${project.properties.svnIgnoreFile} to project ${task.project.name}.")
+            }
+        }
+
         project.task("buildGradleForJavaProject",type: ExportGradleBuildFileForNewJavaProject) { ExportGradleBuildFileForNewJavaProject task ->
             task.parent = task.project.projectDir
             task.projectName = task.project.name
