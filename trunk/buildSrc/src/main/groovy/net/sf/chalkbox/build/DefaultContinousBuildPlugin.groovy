6bb63eb5ce6086529c84110bf3503e79e53c3119
gsande <gsande@3dc70175-81dd-419d-beed-10747ed95c4d>
2013-04-09 11:47:42 +0000

    Continous and DefaultContinous build plugins

---
+++ trunk/buildSrc/src/main/groovy/net/sf/chalkbox/build/DefaultContinousBuildPlugin.groovy
@@ -0,0 +1,20 @@
+package net.sf.chalkbox.build
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class DefaultContinousBuildPlugin implements Plugin<Project>{
+
+    @Override
+    public void apply(final Project project) {
+        project.plugins.apply(JavaProjectArtifactPlugin)
+        project.plugins.apply(FindbugsWithHtmlReports)
+        project.plugins.apply(ForkPmdSettings)
+        project.plugins.apply(JDependWithXmlReports)
+        project.plugins.apply(CodeAnalysis)
+        project.plugins.apply(ContinousPlugin)
+        project.plugins.apply(ForkEclipseJdtPlugin)
+        project.plugins.apply(EclipseClasspathPlugin)
+        project.plugins.apply(ProjectVersion)
+    }
+}
