adebc8796fa5d2f15c0d66266544b0c99a81ddd6
gsande <geir.sande@luukku.com>
2013-04-09 08:50:32 +0000

    ProjectVersion plugin

---
+++ buildSrc/src/main/groovy/net/sf/chalkbox/build/ProjectVersion.groovy
@@ -0,0 +1,16 @@
+package net.sf.chalkbox.build
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ProjectVersion implements Plugin<Project>{
+
+    @Override
+    public void apply(final Project project) {
+        project.task("showVersion") {
+            group 'Miscelleneous'
+            description 'Prints out the version.'
+            doLast {  println project.version}
+        }
+    }
+}
