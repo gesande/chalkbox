6b33d156f8c4386373a778cb9f82880412ed062a
gsande <geir.sande@luukku.com>
2013-04-19 07:20:40 +0000

    examples modified

--- example/src/main/java/net/sf/chalkbox/ChalkExample.java
+++ example/src/main/java/net/sf/chalkbox/ChalkExample.java
@@ -1,16 +1,8 @@
 package net.sf.chalkbox;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.charset.Charset;
 
 public class ChalkExample {
-    public static void main(String[] args) throws IOException {
-        chalk();
-    }
-
-    public static void chalk() throws IOException {
+    public static void main(String[] args) {
         final ChalkBox box = new ChalkBox();
         final StringBuilder sb = new StringBuilder();
         appendText(sb, box.black(), "black");
@@ -21,18 +13,7 @@ public class ChalkExample {
         appendText(sb, box.magenta(), "magenta");
         appendText(sb, box.cyan(), "cyan");
         appendText(sb, box.white(), "white");
-
-        final FileOutputStream out = new FileOutputStream(new File("./target",
-                "chalk-example"));
-        final String content = sb.toString();
-        System.out.println(content);
-        try {
-            out.write(content.getBytes(Charset.defaultCharset()));
-        } finally {
-            out.flush();
-            out.close();
-        }
-
+        System.out.println(sb.toString());
     }
 
     private static void appendText(final StringBuilder sb, final Chalk chalk,
